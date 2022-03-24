c78bf7cb38749fa0938eef20b2e7aa4d8d3ef2d2
cdfoye <cdfoye@gmail.com>
2022-03-24 17:17:29 -0600

    Timeblocks added

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,21 @@
+
+
 // Display today's date
 var now = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(now);
+
+var currentHour = moment().format("H");
+
+console.log(currentHour);
+
+for (var i = 9; i < 18; i++) {
+    var timeBlock = $(i);
+
+    if (timeBlock === currentHour) {
+        timeBlock.addClass("present");
+    } else if (timeBlock < currentHour) {
+        timeBlock.addClass("past");
+    } else {
+        timeBlock.addClass("future");
+    }
+}
